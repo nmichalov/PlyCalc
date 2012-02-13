1b0d5352aff50cec7f3da7ded208fc658116e590
Nathan Michalov <nmichalov@gmail.com>
2012-02-13 18:23:29 -0500

    Added Exponential with ^ as operator

--- PlyCalc.py
+++ PlyCalc.py
@@ -1,37 +1,55 @@
+#!/usr/bin/env python
+"""
+A python parser calculator
+"""
+
 from ply import lex, yacc
 
+equation = raw_input("enter equation:  ")
+
+#the elements of the lexicon
 tokens = (
     "NUMBER",
-    "PLUS",
-    "MINUS",
-    "TIMES",
-    "DIVIDE"
-)
+    "PLUS", "MINUS",
+    "TIMES", "DIVIDE",
+    "EXP",
+    "LPAREN", "RPAREN",
+    )
 
+#definitions of each element
+t_PLUS = r"\+"
+t_MINUS = r"\-"
+t_TIMES = r"\*"
+t_DIVIDE = r"\/"
+t_EXP = r"\^"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+
+#ignore whitespace
+t_ignore = " \t"
+
+#if a token is a number, assign it the proper value
 def t_NUMBER(t):
     r"([1-9][0-9]+)|[0-9]"
     t.value = int(t.value)
     return t
 
-t_PLUS = r"\+"
-t_MINUS = r"\-"
-t_TIMES = r"\*"
-t_DIVIDE = r"\/"
-t_ignore = ' \t'
-
+#raise error if object is not in the lexicon
 def t_error(t):
     raise TypeError("Unkown text '%s'" % (t.value,))
 
+#build tokenizer
 lex.lex()
 
-
+#assign operator precedence (least to greatest)
 precedence = (
     ('left', 'PLUS', 'MINUS'),
     ('left', 'TIMES', 'DIVIDE'),
+    ('left', 'EXP'),
     ('right', 'UMINUS')
     )
 
-
+#the parser functions
 def p_expression_num(p):
     "expression : NUMBER"
     p[0] = p[1]
@@ -46,6 +64,7 @@ def p_expression_binop(p):
                | expression MINUS expression
                | expression TIMES expression
                | expression DIVIDE expression
+               | expression EXP expression
     """
     if p[2] == "+":
         p[0] = p[1] + p[3]
@@ -55,9 +74,16 @@ def p_expression_binop(p):
         p[0] = p[1] * p[3]
     elif p[2] == "/":
         p[0] = p[1] / p[3]
+    elif p[2] == "^":
+        p[0] = p[1] ** p[3]
+
+def p_expression_group(p):
+    "expression : LPAREN expression RPAREN"
+    p[0] = p[2]
 
 def p_error(p):
     print "Syntax error at '%s'" % p.value
 
+#build parser
 yacc.yacc()
-print yacc.parse("25  +-6 * 10")
+print yacc.parse(equation)
